a03d95e54ffa0e3ea58aaffcd81230f11b750bdb
Nick Mumero <mumeronick@gmail.com>
2019-08-21 16:26:11 +0300

    Added funtionality

--- dataViz.py
+++ dataViz.py
@@ -6,9 +6,13 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
 import plotly.graph_objects as go
-from datetime import datetime
+from collections import OrderedDict, Counter
 ska=pd.read_csv("roles.csv")
 techSkills=pd.read_csv("tech_skillz.csv")
+skillCount=techSkills['tech_skill'].value_counts()
+skillDf=pd.DataFrame(skillCount).reset_index()
+skillDf.columns=["tech_skill","count"]
+skills=list(skillDf["tech_skill"])
 
 r=list(ska["Role"].value_counts().index)
 r.sort()
@@ -41,9 +45,16 @@ app.layout = html.Div([
         html.H2(
         children='Brave Venture Labs',
         style={'text-align': 'center',
-                "font":"Roboto",
+                "font":"Lustria",
                 "color":"#09ACF7",
                 "size":"20"}
+    ),
+        html.H3(
+        children='Search by Role',
+        style={'text-align': 'center',
+                "font":"Lustria",
+                "color":"#5B717A",
+                "size":"17"}
     ),
     html.Div([
         html.Div([
@@ -67,7 +78,34 @@ app.layout = html.Div([
                         dcc.Markdown(id="hot_tech",style={'color': "#5B717A",
                                                              'fontSize': 16,
                                                              "font":"Roboto"})
-            ],style= {'width': '49%', 'display': 'inline-block','vertical-align': 'top'})
+            ],style= {'width': '49%',
+                     'display': 'inline-block',
+                     'vertical-align': 'top',
+                     'horizontal-align':"right"})
+    ]),
+    html.Div([
+        html.Div([
+            html.H3(
+                children='Search by Skill',
+                style={'text-align': 'center',
+                        "font":"Lustria",
+                        "color":"#5B717A",
+                        "size":"17"}
+                    ),
+            dcc.Dropdown(
+                        id='Skill Chooser',
+                        options=[{'label': i, 'value': i} for i in skills],
+                        multi=True,
+                        value='Python',
+                    ),
+            html.H4(id="skill chooser" ,
+                    style={'text-align': 'center',
+                                        "font":"Roboto",
+                                        "color":"#09ACF7",
+                                        "size":"16"}),
+            dcc.Graph(id="roles-appearing")
+            
+        ],style= {'width': '49%', 'display': 'inline-block'})
     ])   
 ])
 
@@ -152,6 +190,39 @@ def getRoleplot(role):
 def getTech(role):
     a=roleDetails(role)   
     return ", ".join(a.hot_tech)
+
+@app.callback(
+    Output("skill chooser","children"),
+    [Input('Skill Chooser','value')]
+)
+
+def getPercentage(skill):
+    if type(skill)=="str":
+        perc=round(float(skillDf[skillDf["tech_skill"]==skill]["count"]/1100)*100,2)
+        out=(skill+" occurs in "+str(perc)+"% of O*Net's roles. The roles are listed below.")
+    else:
+        perc=round(float(skillDf[skillDf["tech_skill"]==skill[0]]["count"]/1100)*100,2)
+        out=(skill[0]+" occurs in "+str(perc)+"% of O*Net's roles. The roles are listed below.")
+    return out
+
+@app.callback(
+    Output("roles-appearing","figure"),
+    [Input('Skill Chooser','value')]
+)
+def getroles(skill_list):
+    s=list()
+    for i in skill_list:
+        skills=list(techSkills[techSkills["tech_skill"]==i]["role_name"])
+        s=s+skills
+        class OrderedCounter(Counter, OrderedDict): 
+            pass
+    mskills = [k for k, v in OrderedCounter(s).items() if v > len(skill_list)-1]
+    fig2 = go.Figure(data=[go.Table(header=dict(values=['Roles']),
+                 cells=dict(values=[mskills]))
+                     ])
+    return fig2
+
+
 
 if __name__ == "__main__":
     app.run_server()
