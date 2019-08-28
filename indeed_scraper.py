ca8436e22bcb45578b08291dd62ac647ca881f6c
Nick Mumero <mumeronick@gmail.com>
2019-08-28 15:44:15 +0300

    indeed scraper included

--- indeed_scraper.py
+++ indeed_scraper.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup as soup
 import requests as req
 import pandas as pd
 import csv
-import time
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -20,17 +19,19 @@ def scrape():
         writer=csv.DictWriter(csvfile,fieldnames=["Role","jobtitle","jobkey","jd"])
         writer.writeheader()
         for i in range(len(data)):
-            page=soup(req.get(data["url"][i]).text,'lxml')
-            text=page.find_all('div',{'id':'jobDescriptionText'})[0].text
-            text=text.strip().replace("\n"," ").replace("'","").replace('"','').replace(":","")
-            writer.writerow({"Role":data["Role"][i],
-                            "jobtitle":data["jobtitle"][i],
-                            "jobkey":data["jobkey"][i],
-                            "jd":text})
-            row=[data["Role"][i],data["jobtitle"][i],data["jobkey"][i],text]
-            sheet.insert_row(row,i+2)
-            time.sleep(1)
-            print(i)
+            try:
+                page=soup(req.get(data["url"][i]).text,'lxml')
+                text=page.find_all('div',{'id':'jobDescriptionText'})[0].text
+                text=text.strip().replace("\n"," ").replace("'","").replace('"','').replace(":","")
+                writer.writerow({"Role":data["Role"][i],
+                                "jobtitle":data["jobtitle"][i],
+                                "jobkey":data["jobkey"][i],
+                                "jd":text})
+                row=[data["Role"][i],data["jobtitle"][i],data["jobkey"][i],text]
+                sheet.insert_row(row,i+2)
+                print(i)
+            except IndexError:
+                pass
 
 if __name__ == "__main__":
     scrape()
