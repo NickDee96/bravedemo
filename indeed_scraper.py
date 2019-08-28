8db65b3452a2d2e1c79f23f669e926c5cacfb5f4
Nick Mumero <mumeronick@gmail.com>
2019-08-28 13:12:54 +0300

    indeed scraper included

---
+++ indeed_scraper.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup as soup
+import requests as req
+import pandas as pd
+import csv
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+def scrape():
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('indeedscraper-f298f5bd5f02.json', scope)
+    client = gspread.authorize(creds)
+
+    data=pd.read_csv("pilotRoles.csv")
+    sheet = client.open('Indeed Jds').sheet1
+    header = ["Role","jobtitle","jobkey","jd"]
+    sheet.insert_row(header,1)
+
+    with open("pilotRolesData.csv","a",newline="") as csvfile:
+        writer=csv.DictWriter(csvfile,fieldnames=["Role","jobtitle","jobkey","jd"])
+        writer.writeheader()
+        for i in range(len(data)):
+            page=soup(req.get(data["url"][i]).text,'lxml')
+            text=page.find_all('div',{'id':'jobDescriptionText'})[0].text
+            text=text.strip().replace("\n"," ").replace("'","").replace('"','').replace(":","")
+            writer.writerow({"Role":data["Role"][i],
+                            "jobtitle":data["jobtitle"][i],
+                            "jobkey":data["jobkey"][i],
+                            "jd":text})
+            row=[data["Role"][i],data["jobtitle"][i],data["jobkey"][i],text]
+            sheet.insert_row(row,i+2)
+            print(i)
+
+if __name__ == "__main__":
+    scrape()
