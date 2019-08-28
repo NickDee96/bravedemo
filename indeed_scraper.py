fdf709e6f19a8825722e19675300cb583b8ce642
Nick Mumero <mumeronick@gmail.com>
2019-08-28 21:30:56 +0300

    minor changes

--- indeed_scraper.py
+++ indeed_scraper.py
@@ -10,16 +10,20 @@ def scrape():
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
     creds = ServiceAccountCredentials.from_json_keyfile_name('indeedscraper-f298f5bd5f02.json', scope)
     client = gspread.authorize(creds)
-
     data=pd.read_csv("pilotRoles.csv")
     sheet = client.open('Indeed Jds').sheet1
-    header = ["Role","jobtitle","jobkey","jd"]
-    sheet.insert_row(header,1)
-
+    try:
+        df=pd.DataFrame(sheet.get_all_records())
+        lastKey=df["jobkey"][len(df)-1]
+        a=int(data[data["jobkey"]==lastKey].index.values)
+    except IndexError:
+        a=0
+        header = ["Role","jobtitle","jobkey","jd"]
+        sheet.insert_row(header,1)
     with open("pilotRolesData.csv","a",newline="") as csvfile:
         writer=csv.DictWriter(csvfile,fieldnames=["Role","jobtitle","jobkey","jd"])
-        writer.writeheader()
-        for i in range(len(data)):
+        #writer.writeheader()
+        for i in range(a+1,len(data)):
             try:
                 page=soup(req.get(data["url"][i]).text,'lxml')
                 text=page.find_all('div',{'id':'jobDescriptionText'})[0].text
